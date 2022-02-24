8602f5210d0aec30242515d1a1a47c4a2cffa14e
Ugur Can Avcu <59328866+ugurcanavcu@users.noreply.github.com>
2022-02-24 15:45:25 -0500

    Editing email address

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">You can also click here to email me!</a>
+          <a href="mailto:ugurcavcu@gmail.com" className="p-text">You can also click here to email me!</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
